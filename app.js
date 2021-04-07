7c2dc11f451ba1ca7e15dde0ad7b9d3e5267759f
00009577-bis <72903295+00009577-bis@users.noreply.github.com>
2021-04-07 21:27:25 +0500

    Changes committed

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require ('express')
+const app = express()
+
+const fs = require('fs')
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+// localhost:8000
+app.get('/', (req, res) => {
+	res.render('home')
+})
+
+app.post('/create', (req, res) => {
+	const title = req.body.title
+	const description = req.body.description
+
+	if (title.trim() == '' && description.trim() == '') {
+		res.render('create', { error: true})
+	} else {
+	  fs.readFile('./data/posts.json', (err, data) => {
+		if (err) throw err
+
+		const posts = JSON.parse(data)
+
+		posts.push({
+		  id: id (),
+		  title: title,
+		  description: description,
+		})
+
+		fs.writeFile('./data/posts.json', JSON.stringify(posts), err => {
+		    if (err) throw err
+
+		    res.render('create', { success: true })
+		    })
+
+
+		})
+	}
+
+})
+
+
+app.get('/api/v1/posts', (req, res) => {
+
+	fs.readFile('./data/posts.json', (err, data) => {
+		if (err) throw err
+
+		const posts = JSON.parse(data)
+
+	    res.json(posts)
+	})
+})
+
+
+app.get('/posts', (req, res) => {
+
+	fs.readFile('./data/posts.json', (err, data) => {
+		if (err) throw err
+
+		const posts = JSON.parse(data)
+
+	    res.render(posts, { posts: posts })
+	})
+	res.render('posts', { posts: posts })
+})
+
+app.get('/posts/:id', (req, res) => {
+	const id = req.params.id
+
+	fs.readFile('./data/posts.json', (err, data) => {
+		if (err) throw err
+
+		const posts = JSON.parse(data)
+
+	    const post = posts.filter(note => note.id == id)[0]
+
+	    res.render('detail', { post: post })
+	})
+
+})
+
+app.listen(8000, err => {
+	if (err) console.log(err)
+
+	console.log('Server is running on port 8000...')
+})
+
+
+function id () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
